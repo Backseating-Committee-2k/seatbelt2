7b3216fbee69e47b2ca2108e19bc93f2e834b626
Michael Gerhold <michael.gerhold@web.de>
2022-12-23 18:23:23 +0100

    implemented lexer stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
+#include "lexer.hpp"
+#include "utils.hpp"
+#include <filesystem>
 #include <fmt/format.h>
+#include <magic_enum.hpp>
+
 
 int main() {
-    fmt::print("Hello, world!\n");
+    const auto filename = std::string{ "test.bs" };
+    const auto source_code = utils::read_text_file(filename).value();
+    const auto tokens = tokenize(filename, source_code);
+    if (tokens.has_value()) {
+        for (const auto& token : *tokens) {
+            fmt::print(
+                    stderr, "{}:{}:{}: {} (\"{}\")\n", token.location.filename(), token.location.line_number(),
+                    token.location.column_number(), magic_enum::enum_name(token.type),
+                    utils::to_string_view(token.location.lexeme())
+            );
+        }
+    } else {
+        fmt::print(
+                stderr, "{}:{}:{}: {} (\"{}\")", tokens.error().location.filename(),
+                tokens.error().location.line_number(), tokens.error().location.column_number(),
+                magic_enum::enum_name(tokens.error().error_code),
+                utils::to_string_view(tokens.error().location.lexeme())
+        );
+    }
 }
---
+++ src/source_location.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "types.hpp"
+#include "utils.hpp"
+#include <cassert>
+#include <functional>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+struct SourceLocation final {
+private:
+    std::string_view m_filename;
+
+private:
+    std::u8string_view m_source_code;
+    std::u8string_view m_lexeme;
+
+public:
+    SourceLocation(std::string_view filename, std::u8string_view source_code, std::u8string_view lexeme)
+        : m_filename{ filename },
+          m_source_code{ source_code },
+          m_lexeme{ lexeme } { }
+
+    [[nodiscard]] auto filename() const {
+        return m_filename;
+    }
+
+    [[nodiscard]] auto source_code() const {
+        return m_source_code;
+    }
+
+    [[nodiscard]] auto lexeme() const {
+        return m_lexeme;
+    }
+
+    [[nodiscard]] usize line_number() const {
+        assert(std::greater_equal{}(m_lexeme.data(), m_source_code.data()));
+        assert(std::less_equal{}(m_lexeme.data() + m_lexeme.length(), m_source_code.data() + m_source_code.length()));
+        auto result = usize{ 1 };
+        for (auto current = m_source_code.data(); current != m_lexeme.data(); ++current) {
+            if (*current == '\n') {
+                ++result;
+            }
+        }
+        return result;
+    }
+
+    [[nodiscard]] usize column_number() const {
+        assert(std::greater_equal{}(m_lexeme.data(), m_source_code.data()));
+        assert(std::less_equal{}(m_lexeme.data() + m_lexeme.length(), m_source_code.data() + m_source_code.length()));
+        const auto up_until_lexeme = m_source_code.substr(
+                0, static_cast<decltype(m_source_code)::size_type>(m_lexeme.data() - m_source_code.data())
+        );
+        const auto newline_index = up_until_lexeme.rfind('\n');
+        const auto found = (newline_index != decltype(up_until_lexeme)::npos);
+        const auto line_start = (found ? m_source_code.data() + newline_index + 1 : m_source_code.data());
+        const auto line_until_lexeme = std::u8string_view{ line_start, m_lexeme.data() };
+
+        return utils::utf8_width(line_until_lexeme).value() + 1;
+    }
+};
