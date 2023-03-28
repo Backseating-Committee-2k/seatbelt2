02baccf585cb921837dc1e6e477237b9fead6f97
Totto16 <tobiausgais@gmail.com>
2023-03-28 17:59:23 +0200

    added grabber.py, and refactored it, so that both windows and linux use it

--- tools/grabber.py
+++ tools/grabber.py
@@ -1,1 +1,10 @@
-# TODO this needs to work on windows
+#!/usr/bin/env python3
+
+import sys
+import os
+
+for filename in os.listdir(sys.argv[1]):
+    if os.path.splitext(filename)[-1] == f".{sys.argv[2]}":
+        basename = os.path.basename(filename)
+        library_name = basename[3 : len(basename) - len(sys.argv[2]) - 1]
+        print(library_name)
